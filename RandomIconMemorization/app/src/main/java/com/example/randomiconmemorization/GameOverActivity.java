dc7eafe70566d076cbc101977c18168b4445cd01
Christopher Budd <cbudd@adventuregang.ca>
2022-04-01 19:07:51 -0400

    Fixing Buttons and game not running

--- RandomIconMemorization/app/src/main/java/com/example/randomiconmemorization/GameOverActivity.java
+++ RandomIconMemorization/app/src/main/java/com/example/randomiconmemorization/GameOverActivity.java
@@ -8,8 +8,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+//import com.google.gson.Gson;
+//import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
