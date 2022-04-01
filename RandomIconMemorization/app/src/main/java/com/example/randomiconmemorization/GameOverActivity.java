c32a119da7ba1aa34a47620407126a58e879e3a7
AndrewidRizk <97995173+AndrewidRizk@users.noreply.github.com>
2022-03-31 23:01:52 -0400

    Continue

--- RandomIconMemorization/app/src/main/java/com/example/randomiconmemorization/GameOverActivity.java
+++ RandomIconMemorization/app/src/main/java/com/example/randomiconmemorization/GameOverActivity.java
@@ -2,21 +2,32 @@ package com.example.randomiconmemorization;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class GameOverActivity extends AppCompatActivity {
     private String currentPlayerName;
     private int currentPlayerPoints;
     private HighScore highScore = new HighScore();
-    private TextView view23;
+    private TextView HighScoreName1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
         currentPlayerName = getIntent().getExtras().getString("playerName");
         currentPlayerPoints = getIntent().getExtras().getInt("playerScore");
-        view23 = findViewById(R.id.textView23);
-        view23.setText(currentPlayerName);
+        HighScoreName1 = findViewById(R.id.HighScoreName1);
+        HighScoreName1.setText(currentPlayerName);
+    }
+    public void HomeButton(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    public void PlayAgainButton(View v) {
+        Intent intent = new Intent(this, GameActivity.class);
+        intent.putExtra("playerName", currentPlayerName);
+        startActivity(intent);
     }
 }
