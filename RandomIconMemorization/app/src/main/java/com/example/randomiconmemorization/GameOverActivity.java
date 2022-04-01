acf3e3ab22c86f8c4a76766114eeffd64e2d34de
AndrewidRizk <97995173+AndrewidRizk@users.noreply.github.com>
2022-04-01 00:42:45 -0400

    Andrew HighScore

--- RandomIconMemorization/app/src/main/java/com/example/randomiconmemorization/GameOverActivity.java
+++ RandomIconMemorization/app/src/main/java/com/example/randomiconmemorization/GameOverActivity.java
@@ -12,15 +12,49 @@ public class GameOverActivity extends AppCompatActivity {
     private int currentPlayerPoints;
     private HighScore highScore = new HighScore();
     private TextView HighScoreName1;
+    private TextView HighScoreName2;
+    private TextView HighScoreName3;
+    private TextView HighScoreName4;
+    private TextView HighScoreName5;
+    private TextView ScoreName1;
+    private TextView ScoreName2;
+    private TextView ScoreName3;
+    private TextView ScoreName4;
+    private TextView ScoreName5;
+    private TextView[] names = new TextView[] { HighScoreName1, HighScoreName2, HighScoreName3, HighScoreName4, HighScoreName5};
+    private TextView[] Scores = new TextView[] { ScoreName1, ScoreName2, ScoreName3, ScoreName4, ScoreName5};
+    private int index = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
         currentPlayerName = getIntent().getExtras().getString("playerName");
         currentPlayerPoints = getIntent().getExtras().getInt("playerScore");
-        HighScoreName1 = findViewById(R.id.HighScoreName1);
-        HighScoreName1.setText(currentPlayerName);
+        Score newPlayer = new Score();
+        newPlayer.add(currentPlayerName, currentPlayerPoints);
+        highScore.addScores(newPlayer);
+        names[0] = findViewById(R.id.HighScoreName1);
+        names[1] = findViewById(R.id.HighScoreName2);
+        names[2] = findViewById(R.id.HighScoreName3);
+        names[3] = findViewById(R.id.HighScoreName4);
+        names[4] = findViewById(R.id.HighScoreName5);
+        Scores[0] = findViewById(R.id.ScoreName1);
+        Scores[1] = findViewById(R.id.ScoreName2);
+        Scores[2] = findViewById(R.id.ScoreName3);
+        Scores[3] = findViewById(R.id.ScoreName4);
+        Scores[4] = findViewById(R.id.ScoreName5);
+        if(index < 5)
+            index ++;
+        for(int i = 0; i < index; i++)
+        {
+            names[i].setText(highScore.getHighScores().get(i).getScores()[0]);
+        }
+        for(int j = 0; j < index; j++)
+        {
+            Scores[j].setText( highScore.getHighScores().get(j).getScores()[1]);
+        }
     }
+
     public void HomeButton(View v) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
